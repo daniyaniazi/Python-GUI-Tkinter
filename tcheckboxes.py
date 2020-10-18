f46d39f948fffa9c51887d001a9946209d3072be
daniyaniazi <farihadania@hotmail.com>
2020-10-18 22:49:48 +0500

    update file

--- tcheckboxes.py
+++ tcheckboxes.py
@@ -14,6 +14,6 @@ check_var=IntVar()
 check_box=Checkbutton(root,text='Accept our terms of services',onvalue=1,offvalue=0,variable=check_var,font=('times new roman',20,'bold')).place(x=20,y=50)
 
 btn=Button(root,text="show",font=('times new roman',10,'bold'),bg='yellow',fg='black',activebackground='green',activeforeground='white',cursor='hand2',command=getData).place(x=100,y=120,relwidth=0.15)
-# SELECT GEDER\
+
 
 root.mainloop()
